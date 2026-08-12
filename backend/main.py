2fa692020f75993f8027c6411b4da80f2e64dc6a
Agung Hutri <agung@qtasmin.com>
2026-08-12 16:41:16 +0700

    Add Recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,40 @@
-# Ask for trip details
-destination = input("Destination: ")
-country = input("Country: ")
-days = int(input("Days: "))
-budget = float(input("Budget: "))
-currency = input("Currency: ")
-travel_month = input("Travel Month: ")
-
-
-# Function to display trip summary
-def print_trip_summary(destination, country, days, budget, currency, travel_month):
-    print("====================")
-    print("KelanaAI")
-    print("====================")
-    print(f"Destination : {destination}")
-    print(f"Country     : {country}")
-    print(f"Days        : {days}")
-    print(f"Budget      : {budget} {currency}")
-    print(f"Currency    : {currency}")
-    print(f"Travel Month: {travel_month}")
-
-
-# Display trip summary
-print_trip_summary(
-    destination,
-    country,
-    days,
-    budget,
-    currency,
-    travel_month
+from services.trip_services import (
+    calculate_daily_budget,
+    get_trip_category,
+    get_transportation
 )
+
+print("==============================")
+print("KelanaAI")
+print("==============================")
+
+destination     = input("Destination : ")
+days            = int(input("Days       : "))
+budget          = float(input("Budget     : "))
+travel_month    = input("Travel Month: ")
+
+daily = calculate_daily_budget(budget, days)
+category = get_trip_category(budget)
+transportation = get_transportation(category)
+
+print()
+print(f"Destination : {destination}")
+print(f"Days        : {days}")
+print(f"Budget      : {budget:.0f} USD")
+print(f"Category    : {category}")
+print(f"Daily Budget: {daily:.0f} USD/Day")
+print(f"Travel Month: {travel_month}")
+
+if travel_month.lower() == "december":
+    season = "Peak Season"
+elif travel_month.lower() == "june":
+    season = "Holiday Season"
+else:
+    season = "Regular Season"
+
+print(f"Season      : {season}")
+
+print()
+print("Recommended Transportation")
+print(f"- {transportation}")
+
